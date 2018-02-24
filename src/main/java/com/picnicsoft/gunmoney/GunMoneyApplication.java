ddff06768f15940534c5eaad7a8e23a71323841e
apmihal <apmihal@gmail.com>
2018-02-24 15:43:31 -0600

    Initial commit

---
+++ src/main/java/com/picnicsoft/gunmoney/GunMoneyApplication.java
@@ -0,0 +1,12 @@
+package com.picnicsoft.gunmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunMoneyApplication.class, args);
+	}
+}
